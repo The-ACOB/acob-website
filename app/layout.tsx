b414eb2c5919c14799ea4203d4f7fe1fe73fed02
Muhammad Hasan Zarif <reachappliedcognitio@gmail.com>
2026-06-21 14:20:28 +0600

    Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next'
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono, Outfit } from 'next/font/google'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
